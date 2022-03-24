f1cb767955bbc5956bc28c28cdfe3b6b04280413
ackarlse <anncharlottkarlsen@ymail.com>
2022-03-24 13:16:08 +0100

    laget actorService som henter actors fra sanity

--- movielibrary/src/components/Actors.js
+++ movielibrary/src/components/Actors.js
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {getActors} from '../utils/services/actorService'
 
 export default function Actors() {
-    const [actorList, setActorList] = useState([
-        {actorid: 1, name: "Tom Hanks"},
-        {actorid: 2, name: "Daniel Radcliffe"},
-        {actorid: 3, name: "Leonardo Di Caprio"}
-    ])
+    const [actorList, setActorList] = useState([])
+
+
+    const getActorData = async () =>{
+        const actors = await getActors()
+        setActorList(actors)
+    }
+
+    console.log(actorList)
+
+    useEffect(()=>{
+        getActorData()
+    }, [])
 
     return(
         <>
             <h1>Actors list</h1>
             <section id="actors">
-                {actorList?.map((actor) => <article className="actor"><figure><img src="https://via.placeholder.com/400x300.png?text=Image+not+found" alt={actor.name} /><h2>{actor.name}</h2></figure><Link className="profile-button" to={"/actors/" + actor.name}>Se profil</Link></article>)}
+                {actorList?.map((actor) => <article key={actor._id} className="actor"><figure><img src={actor.profile_image} alt={actor.full_name} /><h2>{actor.full_name}</h2></figure><Link className="profile-button" to={"/actors/" + actor.slug}>Se profil</Link></article>)}
             </section>
         </>
     )
