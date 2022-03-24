b11433cb9756e290c9483331548e423a3216b84f
ackarlse <anncharlottkarlsen@ymail.com>
2022-03-24 13:32:58 +0100

    la til en sjekk paa actors bildene

--- movielibrary/src/components/Actors.js
+++ movielibrary/src/components/Actors.js
@@ -21,7 +21,7 @@ export default function Actors() {
         <>
             <h1>Actors list</h1>
             <section id="actors">
-                {actorList?.map((actor) => <article key={actor._id} className="actor"><figure><img src={actor.profile_image} alt={actor.full_name} /><h2>{actor.full_name}</h2></figure><Link className="profile-button" to={"/actors/" + actor.slug}>Se profil</Link></article>)}
+                {actorList?.map((actor) => <article key={actor?._id} className="actor"><figure><img src={actor.profile_image ? actor.profile_image : "https://via.placeholder.com/300x500.png?text=No+poster"} alt={actor?.full_name} /><h2>{actor?.full_name}</h2></figure><Link className="profile-button" to={"/actors/" + actor?.slug}>Se profil</Link></article>)}
             </section>
         </>
     )
