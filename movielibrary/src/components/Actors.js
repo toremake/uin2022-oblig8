fd46880d71fdde47e48cca6569f3f5513779f9b0
ackarlse <anncharlottkarlsen@ymail.com>
2022-03-24 15:04:11 +0100

    Endret paa actors

--- movielibrary/src/components/Actors.js
+++ movielibrary/src/components/Actors.js
@@ -26,7 +26,7 @@ export default function Actors() {
             <h1>Actors list</h1>
             <section id="actors">
                 {/* {actorList?.map((actor) => <article className="actor"><figure><img src="https://via.placeholder.com/400x300.png?text=Image+not+found" alt={actor.name} /><h2>{actor.name}</h2></figure><Link className="profile-button" to={"/actors/" + actor.name}>Se profil</Link></article>)} */}
-                {actorList?.map((actor) => <article className="actor"><figure><img src="https://via.placeholder.com/400x300.png?text=Image+not+found" alt={actor.name} /><h2>{actor.name}</h2></figure><Link className="profile-button" to={"/actors/" + actor.slug}>Se profil</Link></article>)} {/* Laget kun for test */}
+                {actorList?.map((actor) => <article key={actor._id} className="actor"><figure><img src={actor.profile_image ? actor.profile_image : "https://via.placeholder.com/400x300.png?text=Image+not+found"} alt={actor.full_name} /><h2>{actor.full_name}</h2></figure><Link className="profile-button" to={"/actors/" + actor.slug}>Se profil</Link></article>)} {/* Laget kun for test */}
             </section>
         </>
     )
