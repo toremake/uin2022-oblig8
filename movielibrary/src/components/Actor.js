af8b2a41088183180192f2b8d8b42adeece0a4cd
Håkon <haakostr@hiof.no>
2022-03-27 15:43:12 +0200

    Logikk på plass i actor. Styling mangler.

--- movielibrary/src/components/Actor.js
+++ movielibrary/src/components/Actor.js
@@ -1,35 +1,45 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { getActor } from "../utils/services/actorService";
+import Loaderellipsis from './Loaderellipsis';
+
 
 export default function Actor(full_name) {
 const {slug} = useParams();
 const [actor, setActor] = useState([])
+const [loading, setLoading] = useState(false)
+
 
 const getTheActor = async (slug) => {
+    setLoading(true);
     const theActor = await getActor(slug);
+    setLoading(false);
     setActor(theActor);
 }
 
 useEffect(() => {
     getTheActor(slug);
 },[slug])
-// Av en eller annen grunn så får vi actor som array. 
-const name = actor[0]?.full_name;
-// Liste med filmer som personen har spilt i. Får vi til å lage nettlenke som peker mot selve filmen?
-const movies = actor[0]?.movies.map((movie) => 
-<li>{movie}</li>
-)
-const profileImage = actor[0]?.imageUrl;
 
-return (
-    <>
-    <h1>{name}</h1>
-    <img src={profileImage} height="240" alt="profile" />
-    <p>har spilt i: </p>
-    <ul>
-        {movies}
-    </ul>
-    </>
+const name = actor?.full_name;
+
+console.log(actor)
+
+
+return(
+    loading ? <Loaderellipsis /> : 
+    <article id="movie">
+        <div className="moviemeta">
+        <p><small><Link to="/actors">Actor Library</Link> > {name}</small></p>
+        <h1>{name}</h1>
+        <h3>Cast</h3>
+        <ul className="cast">
+            {actor.movies?.map((movie) => (<li key={movie.slug}><Link to={'/movies/' + movie?.slug}><img src={movie.poster ? movie.poster : "https://via.placeholder.com/100x100.png?text=No+image"} alt={movie.title} /><span>{movie.title}</span></Link></li>))}
+        </ul>
+        </div>
+        <img className="poster" src={actor.imageUrl ? actor.imageUrl : "https://via.placeholder.com/300x500.png?text=No+poster"} alt={actor.full_name} />
+        
+        
+    </article>
 )
 }
--- movielibrary/src/utils/services/actorService.js
+++ movielibrary/src/utils/services/actorService.js
@@ -3,9 +3,9 @@ import client from "../sanityclient";
 const actorFields = `
 _id,
 full_name,
-movies,
+"movies": *[_type == "movie" && references(^._id)]{title, "poster": poster.asset->url, "slug": slug.current},
 "slug": slug.current,
-imageUrl,
+"imageUrl": profile_image.asset -> url,
 `
 
 const actorsFields = `
@@ -17,15 +17,10 @@ full_name,
 
 export const getActors = async () => {
   const data = await client.fetch(`*[_type == "actor"] |order(full_name) {${actorsFields}}`);
-  return data
+  return data;
 }
 
 export const getActor = async (slug) => {
-    const data = await client.fetch(`*[_type == "actor" && slug.current == $slug]{
-        _id, full_name, profile_image,
-        "movies": *[_type == "movie" && references(^._id)].title, 
-        "imageUrl": profile_image.asset->url,
-        
-      }{${actorFields}}`, {slug});
-      return data;
+    const data = await client.fetch(`*[_type == "actor" && slug.current == $slug]{${actorFields}}`,{slug});
+      return data?.[0];
 }
